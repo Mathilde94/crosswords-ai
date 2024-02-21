eebc5221325cf00acbf8793c1e55101cc6f1fb67
Caron <mathilde.caron@udemy.com>
2024-02-20 18:01:22 -0800

    Middleware CORS

--- crosswords/main.py
+++ crosswords/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from crosswords.controllers.routers import routers
 from dotenv import load_dotenv
 
+allowed_origins = [
+    "*",  # webpack server
+]
 
 load_dotenv()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 for router in routers:
     app.include_router(router)
